0b5dc088158072d4773599b00a3dd7febc84520e
saitejakuruva <saitejaplr7@gmail.com>
2020-06-20 12:53:29 +0530

    Set up the things

---
+++ app/config/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "backend123",
+    DB: "backend",
+    dialect: "mysql"
+}
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+}
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/',(req, res) => {
+    res.json({message: "Welcome to my backend hustle"});
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}.`);
+});
